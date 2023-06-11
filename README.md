586e13c42ca190406d061ed328d9ab63270bced6
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-06-11 11:36:05 -0700

    added user prompts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 10-SVG-Logo-Maker
+
+
+____
+[Web Dev Simplified YouTube - Learn Express JS in 35 Minutes](https://www.youtube.com/watch?v=SccSCuHhOw0&ab_channel=WebDevSimplified)
---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path')
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'shape',
+            message: "What shape do you want your logo to be?",
+            choices: [ 'triangle', 'circle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'background-color',
+            message: 'What color background?'
+        },
+        {
+            type: 'input',
+            name: 'font-color',
+            message: 'What color font?'
+        }
+        {
+            type: 'input',
+            name: 'initials',
+            message: 'What initials (up to three)?'
+        }
+
+    ])
+    .then ((answers) => {
+        //use user feedback for whatever"
+    })
+    .caths((error) => {
+        if(error.isTtyError) {
+            //prompt couldn't be rendered in the current environment
+        } else {
+            //something else went wrong
+        }
+    })
